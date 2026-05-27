26f49d5357194e51e2de9426e715f304cbccdcaf
mahaanco <office@camaha.co.in>
2026-05-27 12:48:25 +0530

    Update variance_analysis.py

--- variance_analysis.py
+++ variance_analysis.py
@@ -8,48 +8,55 @@ class VarianceAnalyzer:
 
         self.threshold = threshold
 
-    def calculate_variance(self, prev_df, curr_df, prev_month, curr_month):
+    def calculate_variance(
+        self,
+        df,
+        prev_month,
+        curr_month
+    ):
 
-        merged = pd.merge(
-            prev_df,
-            curr_df,
-            on="GL",
-            how="outer",
-            suffixes=("_prev", "_curr")
-        ).fillna(0)
+        result = pd.DataFrame()
 
-        merged.rename(
-            columns={
-                "Amount_prev": prev_month,
-                "Amount_curr": curr_month,
-            },
-            inplace=True,
+        result["GL"] = df["GL"]
+
+        result[prev_month] = df[prev_month]
+
+        result[curr_month] = df[curr_month]
+
+        result["Variance Amount"] = (
+            result[curr_month]
+            - result[prev_month]
         )
 
-        merged["Variance Amount"] = (
-            merged[curr_month] - merged[prev_month]
-        )
-
-        merged["Variance %"] = np.where(
-            (merged[prev_month] == 0) &
-            (merged[curr_month] == 0),
+        result["Variance %"] = np.where(
+            (
+                result[prev_month] == 0
+            ) &
+            (
+                result[curr_month] == 0
+            ),
             0,
 
             np.where(
-                merged[prev_month] == 0,
+                result[prev_month] == 0,
                 np.nan,
 
                 (
-                    (merged[curr_month] - merged[prev_month])
-                    / merged[prev_month]
+                    (
+                        result[curr_month]
+                        - result[prev_month]
+                    )
+                    / result[prev_month]
                 ) * 100
             )
         )
 
-        merged["Variance Type"] = np.where(
-            merged["Variance %"].abs() >= self.threshold,
+        result["Variance Type"] = np.where(
+            result["Variance %"].abs()
+            >= self.threshold,
+
             "High Variance",
             "Normal"
         )
 
-        return merged
+        return result
